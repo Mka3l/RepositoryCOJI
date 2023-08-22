2dbba3c94e57982c5cb131cfa5a73ff11cab36a8
FenoRandria <117942753+FenoRandria@users.noreply.github.com>
2023-08-22 19:04:04 +1400

    chore: integration delegations kely

--- src/Starter/Composants/Final1/DelegationEnsemble.jsx
+++ src/Starter/Composants/Final1/DelegationEnsemble.jsx
@@ -6,7 +6,7 @@ import url from '../../urlHtpp';
 import DelegationAfficherPerson from './DelegationAfficherPerson';
 const DelegationEnsemble = () => {
 
-  var urlHtpp = "http://127.0.0.1:9090/";
+  var urlHtpp = url;
 
   const NameDelegation = [
     { "nom": "Madagascar" }, { "nom": "Maurice" },
@@ -79,12 +79,54 @@ const DelegationEnsemble = () => {
           padding: '30px',
           background:' #973116',
           textAlign: 'center',
-        }}>Répartition du nombre de personnes par Délégation (nombre total 4 164)</h2>
+        }}>Répartition du nombre de personnes par Délégation (nombre total {repartitionTotal})</h2>
       <div style={{ marginBottom: '50px' }}>
 
         <DelegationClic/> 
         <DelegationAfficherDetails/>
         <DelegationAfficherPerson/>
+        <AjoutHebergement Action={"Ajout Delegation"} Titre={"Ajout Hebergement"}>
+          <p>Votre Contact ID</p>
+          <p> <input type='text' ref={contact_id} className='form-control' required /></p>
+          <p>Nom complet</p>
+          <p> <input type='number' ref={nom_complet} className='form-control' required /></p>
+          <p>Nom categorie</p>
+          <p> <input type='text' ref={nom_categorie} className='form-control' /></p>
+          <p>Nom pays</p>
+          <select name="" ref={nom_pays} id="">
+            <option value="1">Madagascar</option>
+            <option value="2">Maldives</option>
+            <option value="3">Mayotte</option>
+            <option value="4">Comore</option>
+            <option value="5">La Reunion</option>
+            <option value="6">Seychelles</option>
+          </select>
+          <p>Nom Fonction</p>
+          <p> <input type='text' ref={nom_fonction} className='form-control' /></p>
+
+          <p>Nom discipline</p>
+          <p> <input type='text' ref={nom_discipline} className='form-control' /></p>
+
+          <p>Nom comission</p>
+          <p> <input type='text' ref={nom_comission} className='form-control' /></p>
+
+          <p><button className='btn btn-success' onClick={AddHebergement}>Ajouter</button></p>
+        </AjoutHebergement>
+
+        	
+        	
+        	
+        	
+        	
+        	
+        	
+        qualite_demandeur	
+        nom_media	
+        titulaire	
+        printing_status	
+        application_status	
+        hebergement	
+        numero_chambre
       </div>
     </div>
   );
